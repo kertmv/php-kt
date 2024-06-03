556ccc9ecdaa7a09a9b28a04638c639d33513564
kertmv <115462783+kertmv@users.noreply.github.com>
2024-06-03 21:54:00 +0300

    Update avaleht.php

--- avaleht.php
+++ avaleht.php
@@ -11,28 +11,26 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
 <script src="https://kit.fontawesome.com/5f11feec13.js" crossorigin="anonymous"></script>
-	
 <link rel="stylesheet" href="kt.css">
 </head>
--->
 <body>
 	<div id="grad" class="jumbotron text-black">
 	<div class="container">
 	<nav class="navbar navbar-expand-lg navbar-light ">
-		<a class="navbar-brand" href="#"><img src="img/image1.png" width="40px"> vare.com</a>
+	<a class="navbar-brand" href="#"><img src="img/image1.png" width="40px"> vare.com</a>
 	<div class="collapse navbar-collapse" id="minuMenyy">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item active">
-				<a class="nav-link" href="#">Avaleht</a>
+			<a class="nav-link" href="#">Avaleht</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Tooted</a>
+			<a class="nav-link" href="#">Tooted</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Kontakt</a>
+			<a class="nav-link" href="#">Kontakt</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Admin</a>
+			<a class="nav-link" href="#">Admin</a>
 			</li>
 			<li class="nav-item">
 			<a class="fa-solid fa-bag-shopping"></a>
@@ -42,9 +40,9 @@
 	</nav>
 	<div class="row">
 			<div class="col">
-				<br><br><br><br><br><br><br>
-				<h1> <b> SUPER ALA- <br> EALISED, KÕIK -20%! </b></h1>
-				<p class="lead">KINDLASTI kasutan bs5'e</p>
+			<br><br><br><br><br><br><br>
+			<h1> <b> jumala head tooted<br> KÕIK -20%! </b></h1>
+				<p class="lead">kasutame bs5</p>
 				<button type="button" class="btn btn-success">Vaata pakkumisi -></button>
 			</div>
 			<div class="col">
@@ -66,14 +64,14 @@
       ?>
 
         <div class="card border-light">
-			<img src="<?php echo $image; ?>" class="card-img-top" height="300" alt="<?php echo $name; ?>">
+		<img src="<?php echo $image; ?>" class="card-img-top" height="300" alt="<?php echo $name; ?>">
          <div class="card-body">
-		 <h5 class="card-title"><?php echo $name; ?></h5>
+		<h5 class="card-title"><?php echo $name; ?></h5>
             <p class="card-text">Hind: <?php echo $price; ?></p>
         </div>
         </div>
 
-		<?php
+<?php
           }
           fclose($file);
         }
