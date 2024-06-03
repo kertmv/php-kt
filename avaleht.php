39344e3cffb834b42b69e5d6f23899b557fc4651
kertmv <115462783+kertmv@users.noreply.github.com>
2024-06-03 21:48:06 +0300

    Update avaleht.php

--- avaleht.php
+++ avaleht.php
@@ -4,30 +4,22 @@
 <meta charset="UTF-8">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-        <script src="https://kit.fontawesome.com/5f11feec13.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/5f11feec13.js" crossorigin="anonymous"></script>
+	
 <link rel="stylesheet" href="kt.css">
 </head>
-<!-- 
-Arko Avarsalu, KT, 4.3.24
-Koosta Bootstrap5 abil samasugune leht 1p
-	- kohandub ja valideerub
-Koosta men, millel klikkides sisu muutub dÃ¼naamiliselt 1p
-	- teiste lehtede sisse pane lihtsalt pealkiri ja tekst
-	- kui lehte ei eksisteeri, siis tuleb vastav teade
-Kasuta massiivi, et avalehel Jumbotroni sees olev sisu kuvatakse suvaliselt (tekst ja pilt) 2p
-Tooted tulevad CSV failist (pilt, nimetus, hind) 2p
-Loo adminni leht, kus admin saab tooteid kustutada ja lisada 4p
 -->
 <body>
 	<div id="grad" class="jumbotron text-black">
 	<div class="container">
 	<nav class="navbar navbar-expand-lg navbar-light ">
-		<a class="navbar-brand" href="#"><img src="img/image1.png" width="40px"> Avarsalu.com</a>
+		<a class="navbar-brand" href="#"><img src="img/image1.png" width="40px"> vare.com</a>
 	<div class="collapse navbar-collapse" id="minuMenyy">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item active">
